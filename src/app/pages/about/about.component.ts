3151b9e7f4f6bc9451d684c0053829dbd4300583
Ayslla Gomes <ayslla.gomes@outlook.com>
2024-04-17 20:55:18 -0300

    [ADD] ajustando o sobre

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -7,8 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+    public about = 'Sobre';
 
-  ngOnInit(): void {}
+    public resume = `
+        Durante os últimos 25 anos, tenho consolidado minha carreira na área de Tecnologia da Informação, enquanto estabeleço minha vida em Brasília. Minha trajetória profissional tem sido marcada por uma ampla experiência no desenvolvimento de software, combinada com um papel ativo como Scrum Master, liderando equipes de desenvolvimento. Estou comprometida em buscar constantemente o aprimoramento profissional e o crescimento pessoal, alimentada por uma paixão pela inovação e pelo progresso tecnológico.
+    `;
+
+    constructor() { }
+
+    ngOnInit(): void {}
 
 }
