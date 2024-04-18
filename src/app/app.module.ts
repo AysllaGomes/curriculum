7baad60e1b6f39192414fd8ef25f3769e28624c1
Ayslla Gomes <ayslla.gomes@outlook.com>
2024-04-18 09:06:04 -0300

    [ADD] Integrando os componentes criados dentro do currículo principal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,37 @@
-import {NgModule} from '@angular/core';
-import {NgOptimizedImage} from "@angular/common";
-import {BrowserModule} from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { NgOptimizedImage } from "@angular/common";
+import { BrowserModule } from '@angular/platform-browser';
 
-import {AppRoutingModule} from "./app.routes";
+import { AppRoutingModule } from "./app.routes";
 
-import {AppComponent} from './app.component';
-import {AboutComponent} from "./pages/about/about.component";
-import {NavbarComponent} from "./pages/navbar/navbar.component";
-import {FooterComponent} from "./pages/footer/footer.component";
-import {ExperienceComponent} from "./pages/experience/experience.component";
-import {CurriculumComponent} from "./pages/curriculum/curriculum.component";
+import { AppComponent } from './app.component';
+import { AboutComponent } from "./pages/about/about.component";
+import { NavbarComponent } from "./pages/navbar/navbar.component";
+import { FooterComponent } from "./pages/footer/footer.component";
+import { LanguagesComponent } from "./pages/languages/languages.component";
+import { ExperienceComponent } from "./pages/experience/experience.component";
+import { CurriculumComponent } from "./pages/curriculum/curriculum.component";
+import { AcademicEducationComponent } from "./pages/academic-education/academic-education.component";
+import { ProfessionalSkillsComponent } from "./pages/professional-skills/professional-skills.component";
 
 @NgModule({
-    declarations: [AppComponent, CurriculumComponent, ExperienceComponent, AboutComponent, NavbarComponent, FooterComponent],
-    imports: [BrowserModule, AppRoutingModule, NgOptimizedImage,],
+    declarations: [
+        AppComponent,
+        AboutComponent,
+        AcademicEducationComponent,
+        CurriculumComponent,
+        ExperienceComponent,
+        FooterComponent,
+        LanguagesComponent,
+        NavbarComponent,
+        ProfessionalSkillsComponent,
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        NgOptimizedImage
+    ],
     providers: [],
-    bootstrap: [AppComponent]
+    bootstrap: [ AppComponent ]
 })
-export class AppModule {
-}
+export class AppModule {}
--- src/app/pages/curriculum/curriculum.component.ts
+++ src/app/pages/curriculum/curriculum.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-    selector: 'app-curriculum',
+    selector: 'app-pages-curriculum',
     templateUrl: './curriculum.component.html',
     styleUrls: ['./curriculum.component.scss']
 })
@@ -12,12 +12,6 @@ export class CurriculumComponent implements OnInit {
     public about = `
         Durante os últimos 25 anos, tenho consolidado minha carreira na área de Tecnologia da Informação, enquanto estabeleço minha vida em Brasília. Minha trajetória profissional tem sido marcada por uma ampla experiência no desenvolvimento de software, combinada com um papel ativo como Scrum Master, liderando equipes de desenvolvimento. Estou comprometida em buscar constantemente o aprimoramento profissional e o crescimento pessoal, alimentada por uma paixão pela inovação e pelo progresso tecnológico.
     `;
-
-    public back = 'Back-End';
-
-    public front = 'Front-End';
-
-    public database = 'Banco de Dados';
 
     public language = 'Idiomas';
 
