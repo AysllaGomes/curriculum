6bc294fc8a9a8b72f894d421270c56ab8e26014e
Ayslla Gomes <ayslla.gomes@outlook.com>
2024-04-18 11:49:31 -0300

    [UPT] SkillsCategory

--- src/app/pages/professional-skills/professional-skills.component.ts
+++ src/app/pages/professional-skills/professional-skills.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Skill } from "../../shared/models/skill.model";
+
 @Component({
     selector: 'app-pages-professional-skills',
     templateUrl: './professional-skills.component.html',
@@ -7,13 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfessionalSkillsComponent implements OnInit {
 
-    public back = 'Back-End';
-
-    public front = 'Front-End';
-
-    public database = 'Banco de Dados';
-
     public professionalSkills = 'Competências Profissionais';
+
+    public skillGroups: Skill[] = [
+        {
+            title: 'Linguagens',
+            skills: ['PhP', 'Java', 'C#', 'CSS3', 'SASS', 'HTML5', 'JavaScript', 'TypeScript', 'GraphQL', 'Kotlin']
+        },
+        {
+            title: 'Frameworks',
+            skills: ['Symfony', 'Laravel', 'Spring', 'Angular 2+', 'Angular JS', 'PrimeNg', 'PrimeFaces', 'Angular Material', 'React', 'Zend', 'Vue']
+        },
+        {
+            title: 'Tecnologia',
+            skills: ['Docker', 'Maven', 'Jenkins', 'iReport', 'Liquibase', 'Bootsrap', 'MySQL', 'MongoDB', 'PostgreSQL', 'Oracle Database', 'Microsoft SQL Server', 'Android Studio', 'Rancher', 'Node.js', 'Grafeno', 'Kafka', 'RabbitMQ', 'Kubernetes']
+        },
+        {
+            title: 'Design',
+            skills: ['Frontend', 'Reponsive', 'CodeDesign', 'UX/UI']
+        },
+    ];
 
     constructor() {}
 
---
+++ src/app/shared/models/skill.model.ts
@@ -0,0 +1,4 @@
+export class Skill {
+    title: string;
+    skills: string[];
+}
