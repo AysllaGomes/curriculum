9d55dace8ef6726e60daaf56549c1829f5f8c0df
Ayslla Gomes <ayslla.gomes@outlook.com>
2024-04-19 09:46:15 -0300

    [UPT] Melhoria a ordenação e estilização da linguagem

--- src/app/pages/languages/languages.component.ts
+++ src/app/pages/languages/languages.component.ts
@@ -12,15 +12,24 @@ export class LanguagesComponent implements OnInit {
     public language: string = 'Idiomas';
 
     public languages: Language[] = [
-        { name: "Português", level: "Nativo" },
-        { name: "Inglês", level: "Intermediário" },
-        { name: "Espanhol", level: "Intermediário" },
-        { name: "Francês", level: "Básico" },
-        { name: "Eslovaco", level: "Básico" }
+        { name: "Português", nameLevel: "Nativo", level: 'C2' },
+        { name: "Inglês", nameLevel: "Intermediário", level: 'B1' },
+        { name: "Espanhol", nameLevel: "Intermediário", level: 'B1' },
+        { name: "Francês", nameLevel: "Básico", level: 'A1' },
+        { name: "Eslovaco", nameLevel: "Básico", level: 'A1' }
     ];
 
-    constructor() {}
+    constructor() { }
 
-    ngOnInit(): void {}
+    ngOnInit(): void {
+        this.languages.sort((a, b): number => {
+            const levels = { 'C2': 6, 'C1': 5, 'B2': 4, 'B1': 3, 'A2': 2, 'A1': 1 };
+            const levelComparison = levels[b.level] - levels[a.level];
+            if (levelComparison === 0) {
+                return a.nameLevel.localeCompare(b.nameLevel);
+            }
+            return levelComparison;
+        });
+    }
 
 }
--- src/app/shared/models/language.model.ts
+++ src/app/shared/models/language.model.ts
@@ -1,4 +1,5 @@
 export class Language {
     name: string;
+    nameLevel: string;
     level: string;
 }
