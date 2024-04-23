14e73da89d3ad803d7470df7d3130aef5c713d4d
Ayslla Gomes <ayslla.gomes@outlook.com>
2024-04-23 20:56:15 -0300

    [UPT] atualizando os certificados online

--- src/app/shared/service/certification.service.ts
+++ src/app/shared/service/certification.service.ts
@@ -12,127 +12,133 @@ export class CertificationService {
                 year: 2012,
                 provider: 'Kadoche',
                 title: 'Corel Draw X4 e AUTOCAD',
-                url: '../../../assets/certifications/Kadoche/Win7_Digitacao_PackOffice_CorelDraw_3DSMax_AutoCAD.pdf'
+                url: 'https://drive.google.com/file/d/1TpxbwdZfgGXYNbN7em464zkk1d0qWtFD/view?usp=drive_link'
             },
             {
                 year: 2013,
                 provider: 'Kadoche',
                 title: 'Montagem e Configuração de Microcomputadores & Configurações de Softwares',
-                url: '../../../assets/certifications/Kadoche/Montagem_e_Configuracao_de_Microcomputadores_e_Instalacoes_de_Softwares.pdf'
+                url: 'https://drive.google.com/file/d/1WCfi50tA8D2z6iq8C148rA9k3a6b4_iA/view?usp=drive_link'
             },
             {
                 year: 2018,
                 provider: 'Udemy',
                 title: 'HTML5, CCS3 e Javascript',
-                url: '../../../assets/certifications/Udemy/HTML_5_CSS_3_Javascript.pdf'
+                url: 'https://www.udemy.com/certificate/UC-A25SJJPO/'
             },
             {
                 year: 2019,
                 provider: 'Udemy',
                 title: 'Firebase',
-                url: '../../../assets/certifications/Udemy/Firebase.pdf'
+                url: 'https://www.udemy.com/certificate/UC-RBSCD7NG/'
             },
             {
                 year: 2019,
                 provider: 'CertiProf',
                 title: 'SCRUM Foundation',
-                url: '../../../assets/certifications/CertiProf/Certificate_SCRUM.pdf'
+                url: 'https://drive.google.com/file/d/1WGaNT5Txv6b8Cmgoi4itL7S4cIzdoHd8/view?usp=drive_link'
             },
             {
                 year: 2021,
                 provider: 'Origamid',
                 title: 'Automação Front com o NPM',
-                url: '../../../assets/certifications/Origamid/Automacao_Front_End_com_NPM.pdf'
+                url: 'https://origamid.com/certificate/37eba3d8'
             },
             {
                 year: 2021,
                 provider: 'Origamid',
                 title: 'CSS com SASS',
-                url: '../../../assets/certifications/Origamid/CSS_SASS.pdf'
+                url: 'https://origamid.com/certificate/acff0849'
             },
             {
                 year: 2021,
                 provider: 'Origamid',
                 title: 'CSS Flexbox',
-                url: '../../../assets/certifications/Origamid/CSS_Flexbox.pdf'
+                url: 'https://www.origamid.com/certificate/60e5c49e/'
             },
             {
                 year: 2022,
                 provider: 'Origamid',
                 title: 'CSS Avançado - Posicionamento',
-                url: '../../../assets/certifications/Origamid/CSS_Avancado_Posicionamento.pdf'
+                url: 'https://origamid.com/certificate/4a327cd4'
             },
             {
                 year: 2022,
                 provider: 'Udemy',
                 title: 'Angular, .NET Core Web API e Angular Material',
-                url: '../../../assets/certifications/Udemy/Angular_.NET_Core_Web_API_e_Angular_Material.pdf'
+                url: 'https://www.udemy.com/certificate/UC-e7f07581-57d6-4058-92a4-b5957aca04bb/'
             },
             {
                 year: 2022,
                 provider: 'Udemy',
                 title: 'Visual Basic .NET',
-                url: '../../../assets/certifications/Udemy/Visual_Basic_.NET.pdf'
+                url: 'https://www.udemy.com/certificate/UC-8b920f6e-2be6-43cd-8bef-0c570ae6953f/'
             },
             {
                 year: 2022,
                 provider: 'Origamid',
                 title: 'CSS Grid Layout',
-                url: '../../../assets/certifications/Origamid/CSS_Grid_Layout.pdf'
+                url: 'https://www.origamid.com/certificate/e6802513/'
             },
             {
                 year: 2022,
                 provider: 'Origamid',
                 title: 'JavaScript 6',
-                url: '../../../assets/certifications/Origamid/JavaScript_ES6.pdf'
+                url: 'https://www.origamid.com/certificate/94334673/'
             },
             {
                 year: 2022,
                 provider: 'Origamid',
                 title: 'Tipografia Avançada',
-                url: '../../../assets/certifications/Origamid/Tipografia_Avancada.pdf'
+                url: 'https://www.origamid.com/certificate/35337941/'
             },
             {
                 year: 2022,
                 provider: 'Origamid',
                 title: 'UI Design Avançado',
-                url: '../../../assets/certifications/Origamid/UI_Design_Avancado.pdf'
+                url: 'https://www.origamid.com/certificate/fa5a49b2/'
             },
             {
                 year: 2022,
                 provider: 'Origamid',
                 title: 'UX Design Heurísticas',
-                url: '../../../assets/certifications/Origamid/UX_Design_Heuristicas.pdf'
+                url: 'https://www.origamid.com/certificate/4588ce5f/'
             },
             {
                 year: 2022,
                 provider: 'Origamid',
                 title: 'Vue.js 2',
-                url: '../../../assets/certifications/Origamid/Vuejs.pdf'
+                url: 'https://www.origamid.com/certificate/a9b1a678/'
             },
             {
                 year: 2022,
                 provider: 'Origamid',
                 title: 'WordPress Como CMS',
-                url: '../../../assets/certifications/Origamid/WordPress_CMS.pdf'
+                url: 'https://www.origamid.com/certificate/17160626/'
             },
             {
                 year: 2022,
                 provider: 'Origamid',
                 title: 'WordPress REST API',
-                url: '../../../assets/certifications/Origamid/WordPress_REST_API.pdf'
+                url: 'https://www.origamid.com/certificate/b8a4417a/'
             },
             {
                 year: 2022,
                 provider: 'Origamid',
                 title: 'Web Design Completo',
-                url: '../../../assets/certifications/Origamid/Web_Design_Completo.pdf'
+                url: 'https://www.origamid.com/certificate/bf5c8e07/'
             },
             {
                 year: 2024,
                 provider: 'Udemy',
                 title: 'OutSystems Reactive',
-                url: '../../../assets/certifications/Udemy/Ayslla_Gomes_Udemy_OutSystems_Reactive.pdf'
+                url: 'https://ibm-learning.udemy.com/certificate/UC-2544c926-7e8c-4e4d-bff6-3b1787ed17df/'
+            },
+            {
+                year: 2022,
+                provider: 'Udemy',
+                title: 'Redux NGRX com Angular',
+                url: 'https://www.udemy.com/certificate/UC-bcac8834-2401-440c-a576-1efe16160ab3/'
             },
         ];
     }
@@ -143,7 +149,7 @@ export class CertificationService {
                 year: 2019,
                 provider: 'CertiProf',
                 title: 'SCRUM Foundation',
-                url: '../../../assets/certifications/CertiProf/Certificate_SCRUM.pdf'
+                url: 'https://drive.google.com/file/d/1WGaNT5Txv6b8Cmgoi4itL7S4cIzdoHd8/view?usp=drive_link'
             },
         ];
     }
